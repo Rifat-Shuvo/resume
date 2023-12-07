2c830cde754adec924348fb569e7e61131abad6e
Rifat-Shuvo <rhshuvo2812@gmail.com>
2023-12-07 14:27:06 +0600

    The upload photo funtionality is removed

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const generateCv = () => {
     const degree = document.getElementById('degree').value
     const phone = document.getElementById('phone').value
 //photo upload
-    const photoInput = document.getElementById('photo')
-    const photo = photoInput.files[0]
-    const previewPhoto = document.getElementById('previewPhoto')
-    if (photo) {
-        const reader = new FileReader()
-        reader.onload = function (e) {
-            previewPhoto.src = e.target.result
-            previewPhoto.style.display = 'block'
-        }
-        reader.readAsDataURL(photo)
-    } 
+    // const photoInput = document.getElementById('photo')
+    // const photo = photoInput.files[0]
+    // const previewPhoto = document.getElementById('previewPhoto')
+    // if (photo) {
+    //     const reader = new FileReader()
+    //     reader.onload = function (e) {
+    //         previewPhoto.src = e.target.result
+    //         previewPhoto.style.display = 'block'
+    //     }
+    //     reader.readAsDataURL(photo)
+    // } 
     console.log(name, email, skills, subject, degree);
     let newWindow = window.open()
 
@@ -32,9 +32,6 @@ const generateCv = () => {
 </head>
 <body>
     <H1 class="text-green-400">CV Of ${name}</H1>
-    
-    <img src="${previewPhoto.src}" alt="user photo">
-    
     <H1> ${email}</H1>
     <H1> ${phone}</H1>
 
