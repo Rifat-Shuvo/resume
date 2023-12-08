2fc11b0649b614332b9532e5165a42cd225e366a
Rifat-Shuvo <rhshuvo2812@gmail.com>
2023-12-08 18:26:05 +0600

    The Cv page is designed

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const generateCv = () => {
     const subject = document.getElementById('subject').value
     const degree = document.getElementById('degree').value
     const phone = document.getElementById('phone').value
+    const project1 = document.getElementById('project1').value
+    const project2 = document.getElementById('project2').value
 //photo upload
     // const photoInput = document.getElementById('photo')
     // const photo = photoInput.files[0]
@@ -22,7 +24,10 @@ const generateCv = () => {
     console.log(name, email, skills, subject, degree);
     let newWindow = window.open()
 
-    let newelement = `<html lang="en">
+    let newelement = `
+    <!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -30,27 +35,37 @@ const generateCv = () => {
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.4.19/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <H1 class="text-green-400">CV Of ${name}</H1>
-    <H1> ${email}</H1>
-    <H1> ${phone}</H1>
 
-    
-     <h1 class="text-xl">Career objectives</h1>
-     
-     <p class="text-lg">Hello, I'm ${name}, highly motivated and results-oriented professional with 5+ years of experience in the industry. Proven ability to ${skills}. Seeking a challenging role where I can utilize my skills and experience to make a significant contribution to the company.</p>
-     
-     <h1 class="text-xl">Education</h1>
-     
-     <p>Name of ${subject} and ${degree}</p>
-     
-     <h1 class="text-xl">Skills & Experties</h1>
-     
-     <p>I have this folowing skills: ${skills}</p>
+<body class="m-5 p-5">
+    <div class="pb-3 border-b-2">
+        <h1 class="text-3xl font-bold">${name}</h1>
+        <h6 class="text-lg text-green-500 font-medium">Full Stack Developer (MERN)</h6>
+        <h3 class="font-bold">Email: ${email}</h3>
+        <h3 class="font-bold">Phone: ${phone}</h3>
+    </div>
+    <div>
+        <h1 class="p-3 border-b-2 text-2xl bg-gray-400 font-bold">Summary</h1>
+        <p class="text-justify font-medium py-4">Highly motivated and detail-oriented web developer with 3 years of experience in building and maintaining dynamic and engaging web applications. Skilled in various front-end and back-end technologies, including HTML, CSS, JavaScript, React, Firebase, Express, MongoDB and Node.js. Eager to leverage my expertise and passion for web development to contribute to a challenging and rewarding role.</p>
+    </div>
+    <div class="mb-4">
+        <h1 class="p-3 text-2xl border-b-2 bg-gray-400 font-bold mb-4">Skills & Expertise</h1>
+        <pre>${skills}</pre>
+    </div>
+    <div class="mb-4">
+        <h1 class="p-3 border-b-2 text-2xl bg-gray-400 font-bold mb-4">Projects</h1>
+        <p class="pb-3">${project1}</p>
+        <p>${project2}</p>
+    </div>
+    <div>
+        <h1 class="p-1 border-b-2 text-2xl bg-gray-400 font-bold">Education</h1>
+        <p class="text-justify font-medium italic py-1">${degree} in ${subject}</p> 
+    </div>
 
-    <button onclick="window.print()" class="btn btn-primary mt-[80vh]">Print now</button>
-     </body>
-</html> `
+    <button onclick="window.print()" class="btn btn-primary mt-10">Print/Download now</button> 
+</body>
 
+</html>
+    `
+{/* <button onclick="window.print()" class="btn btn-primary mt-[80vh]">Print now</button> */}
     newWindow.document.write(newelement)
 }
